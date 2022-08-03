9cff00c18501d84539c96c15921263374417b82e
Ikram Patah <ikram.patah@rebilly.com>
2022-08-03 15:07:48 +0800

    Use  js-sdk instead of express

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
+const RebillyAPI = require("rebilly-js-sdk").default;
 const express = require("express");
-const axios = require("axios");
 const app = express();
 const port = 3000;
-
-require("dotenv").config();
-const { API_SECRET_KEY, ORGANIZATION_ID, WEBISTE_ID } = process.env;
 
 app.use(express.static("client"));
 app.use(express.json());
 
-axios.defaults.headers.common["REB-APIKEY"] = API_SECRET_KEY;
-axios.defaults.baseURL = `https://api-sandbox.rebilly.com/organizations/${ORGANIZATION_ID}`;
+require("dotenv").config();
+const { API_SECRET_KEY, ORGANIZATION_ID, WEBISTE_ID } = process.env;
+
+const api = RebillyAPI({
+  apiKey: API_SECRET_KEY,
+  organizationId: ORGANIZATION_ID,
+  sandbox: true,
+});
 
 app.post("/create-transaction", async (req, res) => {
   try {
@@ -21,20 +24,24 @@ app.post("/create-transaction", async (req, res) => {
       amount,
     } = req.body;
 
-    const { data: customerFields } = await axios.post("/customers", {
-      paymentToken,
-      primaryAddress,
+    const { fields: customer } = await api.customers.create({
+      data: {
+        paymentToken,
+        primaryAddress 
+      },
     });
 
-    const { data: transactionFields } = await axios.post("/transactions", {
-      type: "sale",
-      websiteId: WEBISTE_ID,
-      customerId: customerFields.id,
-      currency,
-      amount,
+    const { fields: transaction } = await api.transactions.create({
+      data: {
+        type: "sale",
+        websiteId: WEBISTE_ID,
+        customerId: customer.id,
+        currency,
+        amount,
+      },
     });
 
-    res.send(transactionFields);
+    res.status(201).send(transaction);
   } catch (error) {
     const message = err.details ?? "Internal Server Error";
     const code = err.status ?? 500;
